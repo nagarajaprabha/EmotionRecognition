bc0bc3e887c6f98bc7dd682cc41970bbda1f7a85
Nagaraja Prabha <nprabha@dstworldwideservices.com>
2016-03-26 21:32:06 -0400

    added random forest  implementation'

--- inputextraction.py
+++ inputextraction.py
@@ -8,26 +8,34 @@ Created on Wed Mar  2 09:36:39 2016
 import arff,numpy as np , pandas as pd 
 from sklearn.ensemble import RandomForestClassifier
 
-dataset = "";
-data = "";
+trainingDataset = "";
+testDataset = "";
+trainingData = "";
+#data_train = '';
+#data_result = '';
+
+#cleaning the data if necessary
 
 
 def loadFile():
-    #TODO add the logic to loop through list of generated arff files;
-    dataset = arff.load(open('input\emo_large.arff','r'));
+    # TODO add the logic to loop through list of generated arff files;
+    trainingDataset = arff.load(open('input\emo_large.arff','r'));
+    #testDataset = arff.load(open('input\testdataSet.arff','r'));
+    
     print("*******************");
-    #print(dataset)
+    print(trainingDataset['attributes']);
     
     #dataset contains two arrays namely 'attributes' and 'data'.'data' is 
     #  a multidimensional array..
-    data=np.array(dataset['data']);
+    trainingData=np.array(trainingDataset['data']);
     
     #Output log;accessing data elements
     #print(data[0][1]);
     
     #accessing attributes
  #   print(len(dataset['attributes']));
-    parse(data);
+    parse(trainingData);
+
 
 
 def parse(data):
@@ -40,17 +48,30 @@ def parse(data):
 #(10, 6555)   
     print(data.shape);
     
-    print(data[0:2,1:3]);
+   # print(data[0:2,1:3]);
     
-    print(data[0:2, 0]);
+    #print(data[0:2, 0]);
     
     print(data[0:3, 6554]);
     #data=np.array(dataset['data']);
-    data = np.delete(data,[0][0])
+    #data = np.delete(data[::,0])
+    print(data.shape);
     #print(data[0]);
-    print(data);
+    print('line 57')
+    data_train = data[::,1:6554]
+    data_result = data[::, 6554]
+    data_result[0:5] = 'amgry';
+    data_result[5:10] = 'sad'
+    data_result[5] = 'happy';
+    print (data_result)
+    print (data_train.shape);
+    print (data_result.shape);
+    forest = RandomForestClassifier(n_estimators = 100);
+    print ('ready for RF');
+    forest = forest.fit(data_train, data_result);
+    output = forest.predict(data_train);
+    print(output);
     #TODO Pass the parsed input data elements to the algorithm
 
 loadFile();
-forest = RandomForestClassifier(n_estimators = 100);
-#print(data[0:3,::]);
+
