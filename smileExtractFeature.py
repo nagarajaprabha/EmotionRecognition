bb13bf3dd646365ee99cb867be86c36c6ed6735d
Nagaraja Prabha <nprabha@dstworldwideservices.com>
2016-03-27 14:45:20 -0400

    trying to automate mp3 to wav

--- smileExtractFeature.py
+++ smileExtractFeature.py
@@ -11,3 +11,19 @@ Created on Wed Mar  2 10:05:35 2016
 #TODO Read/Create inpput directory
 #TODO Execute the SMILEEXtract command on the input files
 #TODO Make&Copy the files into target folder
+
+
+from pydub import AudioSegment;
+
+from glob import glob
+from pydub import AudioSegment
+import os
+
+os.chdir('input\\angry\\');
+
+for mp3_file in list(glob("*.mp3")):
+
+    song = open(mp3_file,"rb")
+    print(song);
+    AudioSegment.from_file(song,format="wav").export(song.name, format='wav')
+   
